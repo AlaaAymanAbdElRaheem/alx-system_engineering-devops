f79b9d46a14d0c2116bd9ffc6083786167ce7148
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-04 02:33:31 +0300

    task 3

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,31 @@
+#custom HTTP header with Puppet
+
+exec { 'update packages':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update packages'],
+}
+
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+exec {'set X-Served-By header':
+  command => "sudo sed -i '/listen 80 default_server/a add_header X-Served-By ${HOSTNAME};' /etc/nginx/sites-available/default",
+  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+}
+
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+}
