dc61d80f2ff5961e926562356f24029109bef880
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-23 23:43:40 +0300

    fixing username attr

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,9 +2,9 @@
 """script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
 
+import csv
 import requests
 import sys
-import csv
 
 
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     tasks = task_response.json()
     user_response = requests.get(url + "users/{}".format(user_id))
     user = user_response.json()
-    user_name = user["name"]
+    user_name = user["username"]
     file_name = "{}.csv".format(user_id)
 
     with open(file_name, mode='w') as csv_file:
